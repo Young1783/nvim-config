9b6e4854125363fee5ec5615b48b7f6b1859e9b8
Young1783 <youngp513@163.com>
2022-02-28 21:52:15 +0800

    update rg & fd for telescope

--- lua/treesitter_config.lua
+++ lua/treesitter_config.lua
@@ -1,9 +1,7 @@
 require("nvim-treesitter.install").prefer_git = true
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {"java", "c", "bash", "cmake", "comment", "commonlisp", "cpp", "css",
-  "elm", "fish","go", "graphql", "html", "javascript", "jsdoc", "json", "json5", "julia",
-  "kotlin", "lua", "php", "python", "regex", "rst", "ruby", "rust", "scss", "toml", "tsx",
-  "typescript", "vim", "vue", "yaml"}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = {"java", "c", "bash", "cmake", "comment", "commonlisp", "cpp", "css", "lua",
+  "fish","html", "javascript", "json", "json5", "regex", "ruby", "scss", "tsx", "typescript", "vim", "vue", "yaml"},
   ignore_install = {}, -- List of parsers to ignore installing
   highlight = {
     additional_vim_regex_highlighting = false,
